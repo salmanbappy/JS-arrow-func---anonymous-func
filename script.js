b05b9dbc2d9993255965144b6317c69a7925b4bf
salmanbappy <msalmanbappy@gmail.com>
2024-12-11 22:45:06 +0600

    Generator function added

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ console.log(`The result is = ${fun1(2, 3)}`);
     console.log("Hello World");
 
 })();
+
+// generator Function
+function* generatorFunction() {
+    yield "first value"
+    yield "second value"
+    yield "third value"
+    yield "fourth value"
+};
+let gf = generatorFunction()
+console.log(gf.next());
+console.log(gf.next());
+console.log(gf.next());
+console.log(gf.next());
+console.log(gf.next());
+
+//Normal Function
+function fun() {
+    console.log(`Hello 1`);
+    console.log(`Hello 2`);
+    console.log(`Hello 3`);
+    console.log(`Hello 4`);
+
+}
