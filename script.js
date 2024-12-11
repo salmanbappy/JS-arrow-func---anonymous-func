c944335435e76a92ecc392df59655d33dd7e54fc
salmanbappy <msalmanbappy@gmail.com>
2024-12-11 22:31:01 +0600

    no name function added

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ console.log(`The result is = ${fun(2, 3)}`);
 //arrow function
 fun1 = (a, b) => a + b;
 console.log(`The result is = ${fun1(2, 3)}`);
+//no name function
+(function autoWelcome() {
+    console.log("Hello World");
+
+})();
