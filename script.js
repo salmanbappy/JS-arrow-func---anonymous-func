7b750a8e154124d7d19526f1ac883178ab5f75eb
salmanbappy <msalmanbappy@gmail.com>
2024-12-11 22:21:45 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//anonymous function
+fun = function (a, b) {
+    return a * b
+}
+console.log(`The result is = ${fun(2, 3)}`);
+//arrow function
+fun1 = (a, b) => a + b;
+console.log(`The result is = ${fun1(2, 3)}`);
